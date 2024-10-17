ef78d3212decf31ae49e7679f72b1e9470fa479e
NataliaStepniak <natalia.stepniak8@gmail.com>
2024-10-17 11:06:39 +0100

    Create config.py

---
+++ workspaces/MNIST_workspace/MNIST_project/config/config.py
@@ -0,0 +1,44 @@
+import csv
+import numpy as np
+# Read CSV file into a list of rows
+with open('mnist_train_small.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    data_list = list(reader)
+# Convert list to an array
+array = np.array(data_list).astype(float)
+# Save the array to a NPZ file
+np.savez('mnist_train_small.npz', array)
+
+def set_config(c):
+    c.input_path = "sample_data/mnist_train_small.npz"
+    c.compression_ratio = 100
+    # c.number_of_columns = 24
+    # c.latent_space_size = 15
+    c.epochs = 100
+    c.early_stopping = False
+    c.early_stopping_patience = 100
+    c.min_delta = 0
+    c.lr_scheduler = True
+    c.lr_scheduler_patience = 50
+    c.model_name = "CFD_dense_AE"
+    c.model_type = "dense"
+    c.custom_norm = True
+    c.l1 = True
+    c.reg_param = 0.001
+    c.RHO = 0.05
+    c.lr = 0.001
+    c.batch_size = 6000
+    c.test_size = 0
+    c.data_dimension = 2
+    c.apply_normalization = False
+    c.extra_compression = False
+    c.intermittent_model_saving = False
+    c.intermittent_saving_patience = 100
+    c.activation_extraction = False
+    c.deterministic_algorithm = False
+    c.compress_to_latent_space = False
+    c.save_error_bounded_deltas = False
+    c.error_bounded_requirement = 1
+    c.convert_to_blocks = False
+    c.separate_model_saving = False
+    # c.custom_loss_function = "loss_function_swae"
