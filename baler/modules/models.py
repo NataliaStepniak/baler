24cd8f9e4d25a9080236c88f7431d1eb687f0ff4
NataliaStepniak <natalia.stepniak8@gmail.com>
2024-10-22 15:31:17 +0100

    Update models.py

--- baler/modules/models.py
+++ baler/modules/models.py
@@ -727,7 +727,7 @@ class TransformerAE(nn.Module):
     def __init__(
         self,
         n_features,
-        in_dim,
+        in_dim=2,
         h_dim=256,
         n_heads=1,
         z_dim=50,   # z_dim is dimension of latent space, replace latent_size variable with z_dim
